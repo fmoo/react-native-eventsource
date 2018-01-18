0db28c5f0483e902d723eeb787fad66fdc59daf2
Peter Ruibal <ruibalp@gmail.com>
2018-01-18 15:57:54 -0800

    Fix build breakage against React-Native v0.53.0
    0.53.0 removed createJSModules, so compiler complains about the
    @Override directive.   Remove since it's no longer necessary anyway.

--- android/src/main/java/com/github/jonnybgod/RNEventSource/RNEventSourcePackage.java
+++ android/src/main/java/com/github/jonnybgod/RNEventSource/RNEventSourcePackage.java
@@ -25,11 +25,6 @@ public class RNEventSourcePackage implements ReactPackage {
     return modules;
   }
 
-  @Override
-	public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-	}
-
 	@Override
 	public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.emptyList();
